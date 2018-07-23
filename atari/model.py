a808595dfd28d8893f3ddf72c94d9d248dc83b69
simeide <simen.eide@finn.no>
2018-07-23 16:19:29 +0200

    seems like a working example generic for atari games

--- atari/model.py
+++ atari/model.py
@@ -8,13 +8,13 @@ ch = 64
 ksize = 4
 class DQN(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_actions):
         super(DQN, self).__init__()
         self.conv1 = nn.Conv2d(4, ch, kernel_size=ksize, stride=2, padding = 0)
         self.conv2 = nn.Conv2d(ch, ch, kernel_size=ksize, stride=2, padding = 0)
         self.conv3 = nn.Conv2d(ch, ch, kernel_size=ksize, stride=2, padding = 0)
         #self.dense1 = nn.Linear(2592, 1024)
-        self.head = nn.Linear(4096, 4)
+        self.head = nn.Linear(4096, num_actions)
 
     def forward(self, x):
         #print(x.shape)
